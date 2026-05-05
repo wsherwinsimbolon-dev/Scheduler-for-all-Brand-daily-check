6156691173e54edd38737ed99e3aa7f665e917b2
William Sherwin Simbolon <williamsherwinsimbolon@ITs-MacBook-Air.local>
2026-05-05 14:01:27 +0700

    Add login OTP test

---
+++ Daily check All Brand/SGM/Login/LoginOTPSalah.py
@@ -0,0 +1,49 @@
+"""
+==============================================
+  PLAYWRIGHT PYTHON - LOGIN BASIC
+  Install dulu: pip install playwright
+                playwright install chromium
+==============================================
+"""
+
+from playwright.sync_api import sync_playwright
+from Locator_Login import LoginSGM
+
+# ─────────────────────────────────────────
+# FUNGSI UTAMA
+# ─────────────────────────────────────────
+def login():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        # Buat objek dulu, url & goto sudah otomatis di dalam __init__
+        sgm = LoginSGM(page)
+        print(f"✓ Membuka: {sgm.url}")
+        page.wait_for_timeout(5000)
+
+        #Input Username
+        sgm.tombolOTPclick()
+        page.wait_for_timeout(5000)
+
+        #Input nomor HP
+        sgm.kolomHP("082281116519")
+        page.wait_for_timeout(5000)
+
+        #Button masuk melalui OTP
+        sgm.buttonClickMasukOTP()
+        page.wait_for_timeout(5000)
+
+        #masukan nomor otp
+        sgm.inputMasukinOTPboxes("123456")
+        page.wait_for_timeout(5000)
+
+        #Click button Kirim OTP
+        sgm.buttonClickOTPSekarang()
+        page.wait_for_timeout(5000)
+        page.screenshot(path="LoginwithOTPsalah.png")
+
+        browser.close()
+
+if __name__ == "__main__":
+    login()
---
+++ Daily check All Brand/SGM/Login/Login_bener.py
@@ -0,0 +1,42 @@
+"""
+==============================================
+  PLAYWRIGHT PYTHON - LOGIN BASIC
+  Install dulu: pip install playwright
+                playwright install chromium
+==============================================
+"""
+
+from playwright.sync_api import sync_playwright
+from Locator_Login import LoginSGM
+
+# ─────────────────────────────────────────
+# FUNGSI UTAMA
+# ─────────────────────────────────────────
+def login():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        # Buat objek dulu, url & goto sudah otomatis di dalam __init__
+        sgm = LoginSGM(page)
+        print(f"✓ Membuka: {sgm.url}")
+        page.wait_for_timeout(5000)
+
+        #Input Username
+        sgm.noHP("082281116519")
+        page.wait_for_timeout(5000)
+
+        #Input Password
+        sgm.pasWort("Tes12345!")
+        page.wait_for_timeout(5000)
+
+        #button Masuk
+        sgm.tombolMasuk()
+        page.wait_for_timeout(5000)
+        #screenshoot
+        page.screenshot(path="sudah_login.png")
+
+        browser.close()
+
+if __name__ == "__main__":
+    login()
